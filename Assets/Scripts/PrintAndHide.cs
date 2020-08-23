9be2378938e3a80bb1d0b897de9fb074db807627
David Belcher <davidjamesbelcher0@gmail.com>
2020-08-23 22:41:41 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -7,16 +7,25 @@ public class PrintAndHide : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        magicNumber = Random.Range(200, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(gameObject.tag == "Red" & i == 100)
+        {
+            this.gameObject.SetActive(false);
+        }
+        if(gameObject.tag == "Blue" & i == magicNumber)
+        {
+            rend.enabled = false;
+        }
         Debug.Log(this.gameObject.name + ": " +  i++);
     }
 
     private int i = 0;
+    private int magicNumber;
 
     public Renderer rend;
 }
