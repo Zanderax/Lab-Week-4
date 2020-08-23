d2145858d276840938b885231c7cb29aa95d534a
David Belcher <davidjamesbelcher0@gmail.com>
2020-08-23 22:33:25 +1000

    90 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -17,4 +17,6 @@ public class ConsolePrint : MonoBehaviour
     }
 
     private int i = 0;
+
+    public Renderer rend;
 }
